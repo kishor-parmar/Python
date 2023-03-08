dca6141faec860bc94d763b830fad336f363f3ab
Kishor Parmar <kishor@parmars.com>
2023-03-08 21:39:43 +0000

    For Statement

---
+++ forStatement.py
@@ -0,0 +1,17 @@
+# Length of strings
+
+words = ["cat", "window", "defenestrate"]
+
+for word in words:
+    print(word, len(word))
+print()
+
+for x in range(6):
+    print(x)
+print()
+
+words = ["Mary", "had", "a", "little", "lamb"]
+for word in range(len(words)):
+    print(word, words[word])
+print()    
+
