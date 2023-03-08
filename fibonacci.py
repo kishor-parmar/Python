ff39dbc1e09a5ed01e98e3174c7caa5e39bfa043
Kishor Parmar <kishor@parmars.com>
2023-03-08 21:42:59 +0000

    Add Function

--- fibonacci.py
+++ fibonacci.py
@@ -1,10 +1,12 @@
 # Fibonacci  Series
 # the sum of the two elements defines the next
 
-num1, num2 = 0, 1
-while num1 < 1000:
-    print(num1, end=', ')
-    num1, num2 = num2, num1 + num2
-#    num1 = num2
-#    num2 = num1 + num2
+def fib(n):
+    num1, num2 = 0, 1
+    while num1 < n:
+        print(num1, end=', ')
+        num1, num2 = num2, num1 + num2
+#       num1 = num2
+#       num2 = num1 + num2
 
+fib(2000)
