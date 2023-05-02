c1a4a54db1ada150a363759dad0646d4f2e2deee
Kishor Parmar <kishor@parmars.com>
2023-05-02 18:46:32 +0100

    loops

---
+++ loops.py
@@ -0,0 +1,12 @@
+for i in [0, 1, 2, 3, 4, 5]:
+    print(i)
+
+for i in range(6):
+    print(i)
+
+names = ["Harry", "Ron", "John"]
+for name in names:
+    print(name)
+
+for character in names[0]:
+    print(character)
