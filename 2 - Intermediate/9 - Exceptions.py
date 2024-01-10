47a5781533f36ddd8630f1ff93010be942546929
Kishor Parmar <kishor@parmars.com>
2024-01-10 10:58:45 +0000

    Exceptions

---
+++ 2 - Intermediate/9 - Exceptions.py
@@ -0,0 +1,52 @@
+# Error and Exceptions
+
+
+class ValueTooHighError(Exception):
+    pass
+
+
+class ValueTooSmallError(Exception):
+    def __init__(self, message, value):
+        self.message = message
+        self.value = value
+
+
+def test_value(x):
+    if x > 100:
+        raise ValueTooHighError("Value is too high")
+    if x < 5:
+        raise ValueTooSmallError("Value is too small", x)
+
+
+def main():
+    try:
+        test_value(4)
+        test_value(200)
+    except ValueTooHighError as e:
+        print(e)
+    except ValueTooSmallError as e:
+        print(e.message, e.value)
+
+
+if __name__ == "__main__":
+    main()
+
+
+"""
+
+# x = -5
+# if x < 0:
+#    raise Exception(f"x should be positive not {x}")
+
+# assert x >= 0, "x is not positive"
+
+try:
+    a = 5 / 0
+except Exception as e:
+    print(e)
+else:
+    print(" All is fine")
+finally:
+    print("Cleaing up...")
+
+"""
