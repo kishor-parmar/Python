6166858b0c545d55c29b5866ae9881ee248ce9cc
Kishor Parmar <kishor@parmars.com>
2024-01-10 14:54:45 +0000

    Logging

---
+++ 2 - Intermediate/10 - Logging.py
@@ -0,0 +1,18 @@
+# Logging
+
+print("*** Different Levels of Logging ***")
+
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    datefmt="%d/%m/%y %H:%M:%S",
+)
+
+logging.debug("this is a debug message")
+logging.info("This is an info message")
+logging.warning("This is a warning message")
+logging.error("This is an error message")
+logging.critical("This is a critical message")
+print()
