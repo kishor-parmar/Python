2502267433fceeccf6ae850c181a0a5f10e3d8ff
Kishor Parmar <kishor@parmars.com>
2024-01-10 22:50:09 +0000

    Numpy

--- 2 - Intermediate/12 - Random_Numbers.py
+++ 2 - Intermediate/12 - Random_Numbers.py
@@ -120,3 +120,45 @@ print("*** choose a random element from a sequence ***")
 a = secrets.choice(list("ABCDEFGHI"))
 print(a)
 print()
+
+# Random Numbers with Numpy
+print("*** Random Numbers with Numpy ***")
+
+import numpy as np
+
+np.random.seed(1)
+# rand(d0,d1,…,dn)
+# generate nd array with random floats, arrays has size (d0,d1,…,dn)
+print("*** generate nd array with random floats, arrays has size (d0,d1,…,dn) ***")
+
+print(np.random.rand(3))
+print()
+
+# reset the seed
+print("*** reset the seed ***")
+np.random.seed(1)
+print(np.random.rand(3))
+print()
+
+# generate nd array with random integers in range [a,b) with size n
+print("*** generate nd array with random integers in range [a,b) with size n ***")
+
+values = np.random.randint(0, 10, (5, 3))
+print(values)
+print()
+
+# generate nd array with Gaussian values, array has size (d0,d1,…,dn)
+# values from standard normal distribution with mean 0.0 and standard deviation 1.0
+print("*** generate nd array with Gaussian values, array has size (d0,d1,…,dn) ***")
+values = np.random.randn(5)
+print(values)
+print()
+
+# randomly shuffle a nd array
+# only shuffles the array along the first axis of a multi-dimensional array
+print("*** randomly shuffle a nd array ***")
+
+arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+np.random.shuffle(arr)
+print(arr)
+print()
