69523c7678a12bec20d1f50b9b0d94e277a44fdf
Kishor Parmar <kishor@parmars.com>
2024-02-20 10:46:20 +0000

    decorators

--- 1 - Original/decorators.py
+++ 1 - Original/decorators.py
@@ -1,8 +1,13 @@
-def announce(f):
+import functools
+
+
+def announce(func):
+
     def wrapper():
         print("Abount to run the function...")
-        f()
+        func()
         print("Done with the function.")
+
     return wrapper
 
 
@@ -10,5 +15,79 @@ def announce(f):
 def hello():
     print("Hello World")
 
+
 hello()
 
+#####################################################################
+
+
+def my_decorator(func):
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        print("Start")
+        result = func(*args, *kwargs)
+        print("End")
+        return result
+
+    return wrapper
+
+
+@my_decorator
+def add5(num):
+    return num + 5
+
+
+result = add5(10)
+print(result)
+
+# print(help(add5))
+# print(add5.__name__)
+#####################################################################
+
+
+def repeat(num_times):
+    def decorator_repeat(func):
+
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+
+            for _ in range(num_times):
+                result = func(*args, **kwargs)
+            return result
+
+        return wrapper
+
+    return decorator_repeat
+
+
+@repeat(num_times=3)
+def greet(name):
+    print(f"Hello {name}")
+
+
+greet("Kish")
+
+#####################################################################
+
+
+class CountCalls:
+
+    def __init__(self, func):
+        self.func = func
+        self.num_calls = 0
+
+    def __call__(self, *args, **kwargs):
+        self.num_calls += 1
+        print(f"This is executed {self.num_calls} times")
+        return self.func(*args, **kwargs)
+
+
+@CountCalls
+def say_hello():
+    print("hello")
+
+
+say_hello()
+say_hello()
+say_hello()
